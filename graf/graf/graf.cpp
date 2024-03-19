4c9f771453188c31290602e75c9e0306d2e5f8de
Berside <144826358+Berside@users.noreply.github.com>
2024-03-19 22:52:44 +0300

    Finally
    Finally

--- graf/graf/graf.cpp
+++ graf/graf/graf.cpp
@@ -1,31 +1,68 @@
 ﻿#include <iostream>
-#include <chrono>
+#include <stack>
+#include <string>
 #include <fstream>
-#include <map>
-#include <random>
+#include <cstdlib> 
 #include <ctime>
+#include <chrono>
 using namespace std;
-
+// рандоманя генерация
 struct Node {
-    char data;
+    string data;
     Node* left;
     Node* right;
-    Node() : data('\0'), left(nullptr), right(nullptr) {}
-    Node(char data) : data(data), left(nullptr), right(nullptr) {}
+    Node() : data(""), left(nullptr), right(nullptr) {}
+    Node(const string& data) : data(data), left(nullptr), right(nullptr) {}
 };
 
-Node* newNode(char data) {
+Node* newNode(const string& data) {
     return new Node(data);
+}
+int findIndex(const std::string& str, int si, int ei) {
+    stack<char> s;
+    for (int i = si; i <= ei; i++) {
+        if (str[i] == '(') {
+            s.push(str[i]);
+        }
+        else if (str[i] == ')') {
+            if (!s.empty() && s.top() == '(') {
+                s.pop();
+                if (s.empty()) {
+                    return i;
+                }
+            }
+        }
+    }
+    return -1;
 }
 
 int countRightLeafNodes(Node* node) {
     if (node == nullptr) return 0;
     int count = 0;
-    if (node->right != nullptr and node->right->left == nullptr and node->right->right == nullptr) {
+    if (node->right != nullptr && node->right->left == nullptr && node->right->right == nullptr) {
         count += 1;
-        cout << "Найден правый листовой узел: " << node->right->data << endl;
+        //cout << "Найден правый листовой узел: " << node->right->data << endl;
     }
     return count + countRightLeafNodes(node->left) + countRightLeafNodes(node->right);
+}
+
+Node* treeFromString(const std::string& str, int si, int ei) {
+    if (si > ei) return nullptr;
+    int num = 0;
+    while (si <= ei && str[si] >= '0' && str[si] <= '9') {
+        num = num * 10 + (str[si] - '0');
+        si++;
+    }
+    Node* root = newNode(std::to_string(num));
+    int index = -1;
+    if (si <= ei && str[si] == '(') {
+        index = findIndex(str, si, ei);
+    }
+    if (index != -1) {
+        root->left = treeFromString(str, si + 1, index - 1);
+        root->right = treeFromString(str, index + 2, ei - 1);
+    }
+    return root;
 }
 
 void printBT(const string& prefix, Node* node, bool isLeft) {
@@ -35,104 +72,69 @@ void printBT(const string& prefix, Node* node, bool isLeft) {
         printBT(prefix + (isLeft ? "|   " : "    "), node->left, false);
     }
 }
-
 void printBT(Node* node) {
     printBT("", node, false);
 }
-
-Node* createTreeFromFile(const string& filename) {
+Node* readTreeFromFile(const std::string& filename) {
     ifstream file(filename);
-    if (!file.is_open()) {
-        cerr << "Не удалось открыть файл " << filename << endl;
+    if (!file) {
+        cerr << "Unable to open file " << filename << endl;
         return nullptr;
     }
-    map<char, Node*> nodeMap;
-    char parent, child, separator;
-    Node* root = nullptr;
-    while (file >> parent >> separator >> child) {
-        if (separator != '-' && separator != '>') {
-            cerr << "Неверный формат данных в файле." << endl;
-            return nullptr;
-        }
-        if (nodeMap.find(parent) == nodeMap.end()) {
-            nodeMap[parent] = newNode(parent);
-        }
-        if (nodeMap.find(child) == nodeMap.end()) {
-            nodeMap[child] = newNode(child);
-        }
-        if (separator == '-') {
-            if (nodeMap[parent]->left == nullptr) {
-                nodeMap[parent]->left = nodeMap[child];
-            }
-            else if (nodeMap[parent]->right == nullptr) {
-                nodeMap[parent]->right = nodeMap[child];
-            }
-            else {
-                cerr << "Ошибка: узел с данными " << parent << " имеет более двух потомков." << endl;
-                return nullptr;
-            }
-        }
-        else if (separator == '>') {
-            if (nodeMap[parent]->right == nullptr) {
-                nodeMap[parent]->right = nodeMap[child];
-            }
-            else {
-                cerr << "Ошибка: узел с данными " << parent << " уже имеет правого потомка." << endl;
-                return nullptr;
-            }
-        }
-        if (root == nullptr) {
-            root = nodeMap[parent];
-        }
-    }
-    return root;
-}
-void writeTreeToFile(Node* node, ofstream& file, string parent = "") {
-    if (node == nullptr) return;
-    string child = string(1, node->data);
-    if (!parent.empty()) {
-        file << parent << "-" << child << "\n";
-    }
-    writeTreeToFile(node->left, file, child);
-    writeTreeToFile(node->right, file, child);
+    string str;
+    getline(file, str);
+    file.close();
+    return treeFromString(str, 0, str.length() - 1);
 }
 
-Node* generateRandomBinaryTree(int n) {
-    if (n == 0) return nullptr;
-    random_device rd;
-    mt19937 gen(rd());
-    uniform_int_distribution<> dis(48, 122);
-    char data = static_cast<char>(dis(gen));
-    Node* root = newNode(data);
-    int leftN = rand() % n;
-    int rightN = n - leftN - 1;
-    root->left = generateRandomBinaryTree(leftN);
-    root->right = generateRandomBinaryTree(rightN);
-    ofstream file("binary_tree.txt");
-    if (!file.is_open()) {
-        cerr << "Failed to open file for writing." << endl;
-        return nullptr;
+void removeNodeAndDescendants(Node*& node) {
+    if (node == nullptr) return;
+    removeNodeAndDescendants(node->left);
+    removeNodeAndDescendants(node->right);
+    if (node->data == "0") {
+        delete node;
+        node = nullptr;
     }
-    writeTreeToFile(root, file);
-    file.close();
-    return root;
+}
+
+void generateRandomBinaryTree(Node*& node, int n) {
+    if (n == 0) return;
+
+    // Randomly decide whether to add a left or right child
+    int leftN = rand() % n;
+
+    // Create a new node with a random value (for simplicity, we'll use "1" for all nodes)
+    node = newNode("1");
+
+    // Recursively generate the left subtree
+    generateRandomBinaryTree(node->left, leftN);
+
+    // Recursively generate the right subtree with the remaining nodes
+    generateRandomBinaryTree(node->right, n - leftN - 1);
 }
 
 int main() {
     setlocale(LC_ALL, "Russian");
-    auto start = chrono::high_resolution_clock::now();
-    Node* root2 = createTreeFromFile("Module.txt");
-    int count2 = countRightLeafNodes(root2);
-    cout << "Count Right Leaf Nodes: " << count2 << endl << endl;
-    auto end = chrono::high_resolution_clock::now();
-    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
-    cout << "Execution time: " << duration << " milliseconds" << endl;
-    printBT(root2);
-    cout << endl;
-    srand(time(0));
-    int n = 15;
-    Node* root = generateRandomBinaryTree(n);
-    int count = countRightLeafNodes(root);
-    cout << "Count Right Leaf Nodes: " << count << endl << endl;
-    printBT(root);
+    Node* root = readTreeFromFile("Module.txt");
+    if (root != nullptr) {
+        removeNodeAndDescendants(root);
+        printBT(root);
+        cout << endl;
+        cout << "Количество правых листовых узлов: " << countRightLeafNodes(root) << endl;
+
+        srand(time(0));
+        Node* root1 = nullptr;
+        int n = 1000000;
+        auto start = chrono::high_resolution_clock::now();
+        generateRandomBinaryTree(root1, n);
+        removeNodeAndDescendants(root1);
+        //printBT(root1);
+
+        cout << endl;
+        cout << "Количество правых листовых узлов: " << countRightLeafNodes(root1) << endl;
+        auto end = chrono::high_resolution_clock::now();
+        auto duration = chrono::duration_cast<chrono::milliseconds>(end - start).count();
+        cout << "Execution time: " << duration << " milliseconds" << endl;
+    }
+    return 0;
 }
